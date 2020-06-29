e19b468c13cd020aee32df33bb6d82b355c9ee17
Andrew Horn <arhorn@smcm.edu>
2020-06-29 15:22:35 -0500

    now checks for id error and shortens array, actually deleting Book

--- book/DeleteBook.go
+++ book/DeleteBook.go
@@ -1,8 +1,13 @@
 package book
 
 // DeleteBook deletes a book from Book
-func (c *Collection) DeleteBook(id int) {
-	index, _ := c.findByID(id)
+func (c *Collection) DeleteBook(id int) error {
+	index, _, err := c.findByID(id)
+	if err != nil {
+		return err
+	}
 	c.Collection[len(c.Collection)-1], c.Collection[index] = c.Collection[index], c.Collection[len(c.Collection)-1]
+	c.Collection = c.Collection[:len(c.Collection)-1]
 	c.Commit()
+	return nil
 }
