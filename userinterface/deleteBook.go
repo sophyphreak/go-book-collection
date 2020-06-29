788202dcc2688c91c19a1b539a597b4b38e5c4fc
Andrew Horn <arhorn@smcm.edu>
2020-06-29 15:23:29 -0500

    added deleteBook function in user interface

--- userinterface/deleteBook.go
+++ userinterface/deleteBook.go
@@ -1,7 +1,28 @@
 package userinterface
 
-import "../book"
+import (
+	"fmt"
+	"strconv"
+
+	"../book"
+)
 
 func deleteBook(c *book.Collection) {
-
+	var userInput string
+	for {
+		fmt.Print("What is the id of the book you want to delete? ")
+		fmt.Scanln(&userInput)
+		id, err := strconv.Atoi(userInput)
+		if err != nil {
+			fmt.Println("Please provide a valid integer.")
+			continue
+		}
+		err = c.DeleteBook(id)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println("Book with id of", id, "successfully deleted.")
+		}
+		break
+	}
 }
