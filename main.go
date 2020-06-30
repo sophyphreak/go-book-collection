a4c0d0e2b8f51859277efd21eac33d5d35c792af
Andrew Horn <arhorn@smcm.edu>
2020-06-30 10:47:49 -0500

    removed commented out code

--- main.go
+++ main.go
@@ -9,5 +9,4 @@ func main() {
 	var collection book.Collection
 	collection.Populate()
 	userinterface.InterfaceRoot(&collection)
-	// collection.AddBook("The Alchemist", "Paulo Coelho", "Some Publisher", 200, 1989)
 }
