6a35edf19a18351a87d26d43ca128782450df965
Andrew Horn <arhorn@smcm.edu>
2020-06-30 10:45:34 -0500

    program now creates collection if it doesn't exist

--- book/Populate.go
+++ book/Populate.go
@@ -8,6 +8,9 @@ import (
 
 // Populate populates a given collection with the json data in collection.json
 func (c *Collection) Populate() {
+	if _, err := os.Stat("collection.json"); os.IsNotExist(err) {
+		os.Create("collection.json")
+	}
 	jsonFile, err := os.Open("collection.json")
 	if err != nil {
 		panic(err)
